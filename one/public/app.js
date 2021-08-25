1a2bb540c899ec714dc2d4ef81982ebd83f71e64
rca-dev45 <rcaDev13@gmail.com>
2021-08-25 00:58:05 -0500

    for loop notes/examples

---
+++ one/public/app.js
@@ -0,0 +1,50 @@
+// let interests = ['soccer','woodworking','tattoos','news'];]
+
+// for(let i =0; i< interests.length; i++){
+//     console.log(interests[i]);
+// }
+
+// for(let thing of interests){
+//     console.log(thing);
+// }
+
+// for(let char of 'kang the conqueror'){
+//     console.log(char.toUpperCase())
+// }
+
+// const magicSquare = [
+//     [2,7,6],
+//     [9,5,1],
+//     [4,3,8]
+// ];
+
+// for(let i =0; i<magicSquare.length; i++){
+//     let sum = magicSquare[i][0] + magicSquare[i][1] + magicSquare[i][2];
+//     console.log(`sum of row ${sum}`);
+//     console.log("---");
+// }
+
+
+// for(let i =0; i<magicSquare.length; i++){
+//     let row = magicSquare[i];
+//     let sum1 = 0;
+//     for(let j=0; j< row.length; j++){
+//         // console.log(row[j]);
+//         sum1 += row[j];
+//     }
+//     console.log(`I just can't stop ${sum1}`);
+// }
+
+
+// const movieReviews ={
+//     Arrival : 9.5,
+//     'Fight Club' : 9,
+//     Magnolia : 5,
+//     'There Will Be Blood' : 8,
+
+// };
+
+// for(let m of Object.keys(movieReviews)){
+//     console.log(m, movieReviews[m]);
+// }
+
