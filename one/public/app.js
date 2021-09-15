6787788284511f0f3f24b8e2f92a0688fdbf0610
rca-dev45 <rcaDev13@gmail.com>
2021-09-15 09:29:31 -0500

    Section Eleven finished

--- one/public/app.js
+++ one/public/app.js
@@ -87,3 +87,126 @@ const tripod = {
     ...canine
 }
 
+const catDogClone = {
+    ...catDog
+}
+
+const random = [...'hello', {...catDog}];
+
+//Rest
+
+// function sum(){
+//     console.log(arguments);
+//     const argsArr = [...arguments]
+//     return argsArr.reduce((total,currVal)=>{
+//         return total + currVals
+//     })
+// }
+
+function fullName(first, last){
+    console.log(arguments);
+    console.log(first);
+}
+
+//arguments doesn't work with arrow functions 
+const multiply = ()=>{
+    console.log(arguments);
+}
+
+function sum(...nums) {
+    return nums.reduce((total,currVal)=>{
+        return total + currVal
+    })
+}
+
+function fullName(first, last, ...titles){
+    console.log("first",first);
+    console.log("last",last);
+    console.log("titles",titles);
+}
+
+const multiply =(...nums)=>(
+     nums.reduce((total,currVal)=>{total * currVal})
+)
+
+//destructuring
+
+const raceResults =[
+    'Eliot Kipe',
+    'Fey Lelisa',
+    'Galen Rupp',
+    'May Helassie',
+    'Al Simbu',
+    'Jared Ward'
+];
+
+// const gold = raceResults[0];
+// const silver = raceResults[1];
+// const bronze = raceResults[2];
+
+
+const [gold,silver,bronze] = raceResults;
+
+const [first,,, fourth]= raceResults;
+
+const [winner,...others]=raceResults;
+
+const runner = {
+    first: 'Eliot',
+    last : 'Kipe',
+    country: "Kenya",
+    title: "Elder of the Order of the Golden Heart of Kenya"
+}
+
+// const {first, last, time}= runner;
+
+// const {country: nation} = runner;
+
+// const {honorific: title} = runner;
+
+const {first, last, ...other}= runner;
+
+const results =[{
+    first: 'Eliot',
+    last : 'Kipe',
+    country: 'Kenya',
+},
+{
+    first: 'Fey',
+    last : 'Lelisa',
+    country: 'Ethiopia'
+},
+{
+    first: 'Galen',
+    last : 'Rupp',
+    country: 'United States'
+}]
+
+
+const [,{country}] =results;
+
+const [{first:goldWinner},{country}] =results;
+
+const [,silverMedal] =results;
+const {country} = silverMedal;
+
+
+// function print(person){
+//     const{first,last,title}=person;
+//     console.log(`${first} ${last}, ${title}`)
+// }
+
+function print({first,last,title}){
+    const{first,last,title}=person;
+    console.log(`${first} ${last}, ${title}`)
+}
+
+const response=[
+    'HTTP/1.1',
+    '200 OK',
+    'application/json',
+]
+
+function parseResponse([protocol,statusCode, contentType]){
+    console.log(`Status: ${statusCode}`)
+}
