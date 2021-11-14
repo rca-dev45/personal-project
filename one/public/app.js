a698378a24b875aba9ae2b50fb82f92befaacca6
rca-dev45 <rcaDev13@gmail.com>
2021-11-13 21:03:11 -0600

    update

--- one/public/app.js
+++ one/public/app.js
@@ -1,6 +1,50 @@
 //Section 13
+//almost all console and html work
 
+//Section 14
+const allLis = document.querySelectorAll('li');
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'indigo',
+    'violet'
+];
 
+const todo = document.querySelector('#todos, .todo');
+
+// todo.style.color = "gray";
+// todo.style.textDecoration = "line-through";
+// todo.style.opacity = "50%";
+
+const newImg = document.createElement('img');
+// newImg.src = "https://domf5oio6qrcr.cloudfront.net/medialibrary/5390/h1218g16207258089583.jpg"
+newImg.style.width = "200px";
+document.body.appendChild(newImg);
+
+const newLink = document.createElement('a');
+newLink.innerText = 'Mr Bubz Video! Click MEEE'
+// newLink.href = 'https://www.youtube.com/watch?v=QQNL83fhWJU';
+
+const firstP = document.querySelector('p');
+firstP.appendChild(newLink);
+
+// for (let i = 0; i<allLis.length; i++){
+//     // console.log(allLis[i].innerText);
+//     allLis[i].innerText = "We are the champions"
+// }
+
+// for(let li of allLis){
+//     li.innerHTML = "We are <b> the Champions</b>"
+// }
+
+allLis.forEach((li, i)=>{
+    const color = colors[i];
+    li.style.color= color;
+})
 
 //Section 15
 const btn = document.querySelector('button');
